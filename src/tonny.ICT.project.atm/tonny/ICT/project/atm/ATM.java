8e388759d8fae23aaaebc4833b6f0c0a04349003
daddy-tonny <fongangtonny87@gmail.com>
2024-08-05 12:37:39 +0100

    ATM

---
+++ src/tonny.ICT.project.atm/tonny/ICT/project/atm/ATM.java
@@ -0,0 +1,8 @@
+package tonny.ICT.project.atm.tonny;
+
+public class ATM extends Optionmenu{ 
+    public static void main(String[] args) {
+        Optionmenu optionmenu =new Optionmenu();
+        optionmenu.getLogin();
+    }
+}
